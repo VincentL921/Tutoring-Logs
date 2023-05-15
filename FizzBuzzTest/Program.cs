0043196f4b454ed1b6b295bf8aef83164994aba8
Vincent Lentini <vincentlentini@Vincents-MacBook-Pro.local>
2023-05-15 19:14:46 -0400

    continuing projects from codeacademy

---
+++ FizzBuzzTest/Program.cs
@@ -0,0 +1,36 @@
+﻿//Create a loop that loops 50 times.
+//For every number divisible by 3, print "fizz".
+//For every number divisible by 5, print "buzz".
+//For every number divisible by both 3 & 5, print "fizz buzz", respectively.
+// If a number is not divisible by either 3 or 5, just print out the number itself.
+
+using System;
+
+namespace FizzBuzzTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int digits = 1;
+            int x = 3;
+            int y = 5;
+
+            for (digits = 1; digits < 51; digits++)
+            {
+                if (digits % x == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+                else if (digits % y == 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+                else
+                {
+                    Console.WriteLine($"{digits}");
+                }
+            }
+        }
+    }
+}
---
+++ JumpStatementsExercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+using System.Reflection;
+
+namespace JumpStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool buttonClick = false;
+
+            do
+            {
+                int counter = 0;
+                counter++;
+                Console.WriteLine("BLARRRRR");
+
+                if (counter == 3)
+                {
+                        break;
+                }
+
+            } while (!buttonClick);
+
+        }
+    }
+}
+ //SHOW DAVE - having trouble on how to break the infinite loop and get it to print out 3 instead of an infinite loop.   
+
