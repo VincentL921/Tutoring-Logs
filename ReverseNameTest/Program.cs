0395a2bcafd2bc71a234e72e975d3197951732b5
Vincent Lentini <vincentlentini@Vincents-MacBook-Pro.local>
2023-05-16 19:38:59 -0400

    continuing projects from codeacademy

---
+++ ReverseNameTest/Program.cs
@@ -0,0 +1,24 @@
+﻿//Create variable for your name and print it backwards.
+//Make sure to get your name to print horizontally, not vertically.
+//**DO NOT USE Array.Reverse**
+
+using System;
+
+namespace ReverseNameTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] name = { "V", "i", "n", "c", "e", "n", "t", " ", "L", "e", "n", "t", "i", "n", "i" };
+
+            foreach (string letter in name.Reverse())
+            {
+                Console.Write($"{letter}");
+            }
+
+        }
+    }
+}
+        
+
