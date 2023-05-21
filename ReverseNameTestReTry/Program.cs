0c14920ed04e13849739ca68bc1d3faac58c3cf3
Vincent Lentini <vincentlentini@Vincents-MacBook-Pro.local>
2023-05-21 18:04:23 -0400

    continuing projects from codeacademy

---
+++ ReverseNameTestReTry/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ReverseNameTestReTry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Vincent Lentini";
+            string reverseName = string.Empty;
+
+            for (int length = name.Length - 1; length >= 0; length--)
+            {
+                reverseName += name[length];
+            }
+            Console.WriteLine(reverseName);
+
+        }     
+
+    }
+}
