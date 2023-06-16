981b27aa4736a1dcaa543f169fba7dd4380b4a59
Vincent Lentini <vincentlentini@vincents-mbp.home>
2023-06-16 18:19:15 -0400

    updated FiveK Project

--- FiveKLogs/Program.cs
+++ FiveKLogs/Program.cs
@@ -11,27 +11,36 @@ namespace FiveKLogs
             string enter = Console.ReadLine();
 
             string[] names = { "Vincent", "Vito", "Gio", "Dave", "Justin" };
-            int[] minutes = { 18, 19, 20, 21, 22 };
-            int[] values = new int[5];
+            int[] minutes = new int[5];
 
             if (names.Length != minutes.Length)
             {
                 Console.WriteLine("Warning: Times are not equal to answers");
             }
 
-            int scoringIndex = 0;
+            int indexOfRunners = 0;
             foreach (string title in names)
             {
                 Console.Write($"How many minutes did {title} run: ");
-                int inputAsInt = int.Parse(Console.ReadLine());
+                string runnerInput = Console.ReadLine();
+                int inputAsInt;
+                bool parsedInput = int.TryParse(runnerInput, out inputAsInt);
 
-                //values[scoringIndex] = inputAsInt;
-                //scoringIndex += 1;
-
-                for (int i = 0; i < inputAsInt; i++)
+                while (!int.TryParse(runnerInput, out inputAsInt))
                 {
-                    Console.WriteLine($"{title} - {inputAsInt} minutes");
-                    break;
+                    Console.WriteLine("Invalid. Please provide a sufficient time");
+                    runnerInput = Console.ReadLine();
+                    parsedInput = int.TryParse(runnerInput, out inputAsInt);
+                }
+
+                minutes[indexOfRunners] = inputAsInt;
+                indexOfRunners += 1;
+
+                for (int i = 0; i < names.Length; i++)
+                {
+                    string runnerNames = names[i];
+                    int runnerTimes = minutes[i];
+                    Console.WriteLine($"{runnerNames} - {runnerTimes} minutes");
                 }
             }
         }
