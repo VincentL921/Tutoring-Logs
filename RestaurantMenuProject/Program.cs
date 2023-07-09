7b17a304e831173b390b6b3cadeb8b39d7acb8d4
Vincent Lentini <vincentlentini@Vincents-MacBook-Pro.local>
2023-07-09 18:56:38 -0400

    Completed/Open for editing Menu exercise

--- RestaurantMenuProject/Program.cs
+++ RestaurantMenuProject/Program.cs
@@ -7,7 +7,7 @@ namespace RestaurantMenuProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Zio Vin's! Please Press Enter to Begin your Order:");
+            Console.WriteLine("Welcome to Vin's Pizza! Please Press Enter to Begin your Order:");
             string enter = Console.ReadLine();
 
             string[] menuItems = { "Pizza", "Pasta", "Chicken Parm", "Meatballs", "Soda" };
@@ -23,18 +23,30 @@ namespace RestaurantMenuProject
             Console.WriteLine($"Please Begin Order:");
             foreach (string title in menuItems)
             {
+                Console.WriteLine("What would you like?");
                 string menuInput = Console.ReadLine();
+                //int answerInput = 1;
                 int inputAsInt;
                 bool parsedInput = int.TryParse(menuInput, out inputAsInt);
 
-                if (inputAsInt < index.Length)
+                while ((inputAsInt > 5) || (!int.TryParse(menuInput, out inputAsInt)))
                 {
                     Console.WriteLine("Invalid Order. Please order a food item within range:");
                     menuInput = Console.ReadLine();
                     parsedInput = int.TryParse(menuInput, out inputAsInt);
                 }
 
+                itemNumbers[indexOfItems] = inputAsInt;
+                indexOfItems += 1;
+                //int translationNumber = 0;
 
+                for (int i = 0; i < indexOfItems; i++)
+                {
+                    //int translationNumber = itemNumbers[inputAsInt];
+                    string translationOfUserInput = menuItems[inputAsInt - 1];
+                    Console.WriteLine($"Your order: {translationOfUserInput}!");
+                }
+                Console.WriteLine("Thank you for your order. Please press Enter to checkout/continue!");
 
             }
 
