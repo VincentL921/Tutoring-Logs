91b2dcea5fc8b2c8729f2142ebdeeac510c493fa
Vincent Lentini <vincentlentini@Vincents-MacBook-Pro.local>
2023-08-17 19:34:36 -0400

    PM work with no methods

--- ProjectManagementWork/JobDescription.cs
+++ ProjectManagementWork/JobDescription.cs
@@ -5,6 +5,7 @@ namespace ProjectManagementWork
 	{
         public string JobTitle { get; set; }
 		public string JobLocation { get; set; }
+		public string CompanyName { get; set; }
         public PaymentAndCompensation paymentAndCompensation { get; set; }
 		public JobQualifications jobQualifications { get; set; }
 		public DailyTasksOfJob dailyTasksOfJob { get; set; }
--- ProjectManagementWork/Program.cs
+++ ProjectManagementWork/Program.cs
@@ -1,18 +1,21 @@
-﻿using System.Collections.Generic;
+﻿
 using System.Linq;
 using ProjectManagementWork;
 
 List<JobDescription> jobPosting = new List<JobDescription>();
 var seniorProjectManager = new JobDescription();
 
+//List<BidProposal> project = new List<BidProposal>();
+//var samsungPennOne = new BidProposal();
+
 seniorProjectManager.paymentAndCompensation = new PaymentAndCompensation();
 seniorProjectManager.jobQualifications = new JobQualifications();
 seniorProjectManager.dailyTasksOfJob = new DailyTasksOfJob();
 
 
-
 seniorProjectManager.JobTitle = "Senior Project Manager";
-seniorProjectManager.JobLocation = "244-250 Green Street, Brooklyn NY 11222";
+seniorProjectManager.CompanyName = "Gotham Drywall Inc.";
+seniorProjectManager.JobLocation = "Various Project Sites & 244-250 Green Street, Brooklyn NY 11222";
 
 seniorProjectManager.paymentAndCompensation.Salary = 120000;
 seniorProjectManager.paymentAndCompensation.Bonus = "You will recieve 10% of total profit made on projects you complete *going to come up with a formula for this one - VL*";
@@ -28,6 +31,65 @@ seniorProjectManager.dailyTasksOfJob.BidLeveling = "You will help evaluate bid o
 seniorProjectManager.dailyTasksOfJob.CostEstimation = "You may, from time to time, be responsible to quantify and estimate the quantities of materials needed for the project.";
 seniorProjectManager.dailyTasksOfJob.PreCon = "You will collaborate in executing the pre-construction phase of a project with the assigned Preconstruction team. This includes conducting a thorough analysis to assess the design of the building as well as all the documentation and reports/surveys needs for this phase.";
 seniorProjectManager.dailyTasksOfJob.DesignPhase = "You will be working with architects and engineers to refine the conceptual design of the drawings. You will also help ensure effective communication and coordination among all design disciplines including architects, engineers, and consultants.";
+
+
+seniorProjectManager.jobQualifications.Education = "Bachelor’s degree in a relevant field such as project management, business administration, engineering, and /or construction management.";
+seniorProjectManager.jobQualifications.YearsOfExperience = 5;
+seniorProjectManager.jobQualifications.SkillsetsForPosition = "Experience in coordinating project activities, assisting the Director of Construction or Senior Project Manager, and collaborate cross- functionally with departments to execute the vision of the project. Ability to manage multiple projects and their tasks, milestones and deliverables.";
+seniorProjectManager.jobQualifications.CommunicationExperience = "Strong communication skills, both written and verbal.";
+seniorProjectManager.jobQualifications.TechnicalExperience = "Proficiency in construction management software, such as Procore-familiarity with project management tool Plexxis a plus.";
+
+
+jobPosting.Add(seniorProjectManager);
+
+
+foreach (var item in jobPosting)
+{
+    Console.WriteLine($"Construction Job Posting in NYC");
+    Console.WriteLine($"Job Title: {item.JobTitle}");
+    Console.WriteLine($"Company Name: {item.CompanyName}");
+    Console.WriteLine($"Location of Work: {item.JobLocation}");
+
+    Console.WriteLine("");
+
+    Console.WriteLine("About The Job:");
+    Console.WriteLine("");
+    Console.WriteLine("Responsibilities:");
+    Console.WriteLine($"- Supporting the Project: {item.dailyTasksOfJob.SupportingTheProject}");
+    Console.WriteLine("");
+    Console.WriteLine($"- Communication & Coordination: {item.dailyTasksOfJob.CommunicationAndCoordination}");
+    Console.WriteLine("");
+    Console.WriteLine($"- Documentation & Reporting: {item.dailyTasksOfJob.DocumentationAndReporting}");
+    Console.WriteLine("");
+    Console.WriteLine($"- Risk Management: {item.dailyTasksOfJob.RiskManagement}");
+    Console.WriteLine("");
+    Console.WriteLine($"- Bid Leveling: {item.dailyTasksOfJob.BidLeveling}");
+    Console.WriteLine($"");
+    Console.WriteLine($"- Cost Estimation: {item.dailyTasksOfJob.CostEstimation}");
+    Console.WriteLine($"");
+    Console.WriteLine($"- PreCon: {item.dailyTasksOfJob.PreCon}");
+    Console.WriteLine($"");
+    Console.WriteLine($"- Design Phase: {item.dailyTasksOfJob.DesignPhase}");
+
+    Console.WriteLine($"");
+
+    Console.WriteLine($"Qualifications:");
+    Console.WriteLine($"- Education: {item.jobQualifications.Education}");
+    Console.WriteLine($"- Experience: {item.jobQualifications.YearsOfExperience} or more years of high rise construction experience is needed.");
+    Console.WriteLine($"- Demands of This Job: {item.jobQualifications.SkillsetsForPosition}");
+    Console.WriteLine($"- Comminucation: {item.jobQualifications.CommunicationExperience}");
+    Console.WriteLine($"- Techincal Proficiency: {item.jobQualifications.TechnicalExperience}");
+
+    Console.WriteLine($"");
+
+    Console.WriteLine($"Payment & Compensation:");
+    Console.WriteLine($"Annual Salary: ${item.paymentAndCompensation.Salary}");
+    Console.WriteLine($"Bonus: {item.paymentAndCompensation.Bonus}");
+    Console.WriteLine($"Vacation: {item.paymentAndCompensation.Vacation} paid for vacation days.");
+    Console.WriteLine($"PTO: {item.paymentAndCompensation.PTO} paid for sick/leave days.");
+
+
+}
 
 
 //Supporting the Project: You will assist the Director of Construction in various tasks, such as managing staff like inspectors, schedulers, estimators, and site safety personnel. Other tasks include coordinating and organizing project plans, scheduling meetings, allocating project resources, and monitoring construction schedule progress. 
